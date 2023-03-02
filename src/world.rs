1cb8944dd7234dc496cf7add5de3987eae6bcc4e
Oleg Kosenko <oleg.kosenko@brainlab.com>
2023-03-02 21:30:01 +0100

    refactor acceleration update

--- src/world.rs
+++ src/world.rs
@@ -1,11 +1,11 @@
-use std::collections::HashMap;
-use std::time::Duration;
-use tokio::sync::watch::{Receiver, Sender};
-use tokio::time::interval;
-use tokio::sync::watch;
 use crate::celestial::Celestial;
 use crate::utils::NormVec3;
 use crate::Vec3;
+use std::collections::HashMap;
+use std::time::Duration;
+use tokio::sync::watch;
+use tokio::sync::watch::{Receiver, Sender};
+use tokio::time::interval;
 
 const G: f64 = 6.6743 as f64 * 0.000_000_000_01;
 const DELTA_T: f64 = 60. * 60.;
@@ -24,7 +24,7 @@ impl World {
                 celestials,
                 world_publisher,
             },
-            world_watch
+            world_watch,
         )
     }
 
@@ -48,9 +48,14 @@ impl World {
         let mut acceleration = Vec3::default();
 
         for (_, celestial) in &self.celestials {
-            let NormVec3 { distance_sq, unit_direction, .. } = (celestial.pos() - &origin).normalize();
+            let NormVec3 {
+                distance_sq,
+                unit_direction,
+                ..
+            } = (celestial.pos() - &origin).normalize();
             if distance_sq > 1. {
-                acceleration += unit_direction * (G * celestial.mass() / distance_sq);
+                acceleration +=
+                    unit_direction * (G * celestial.mass() / distance_sq);
             }
         }
 
@@ -58,17 +63,14 @@ impl World {
     }
 
     fn update_celestials(&mut self) {
-        let cloned_keys: Vec<String> = self.celestials.keys().cloned().collect();
-        let mut accelerations = HashMap::new();
-        for key in &cloned_keys {
+        let mut accelerations = HashMap::with_capacity(self.celestials.len());
+        for key in self.celestials.keys() {
             let a = self.get_global_acceleration(self.celestials[key].pos());
             accelerations.insert(key.clone(), a);
         }
-        for key in &cloned_keys {
-            if let Some(celestial) = self.celestials.get_mut(key) {
-                if let Some(a) = accelerations.get(key) {
-                    celestial.apply_gravity(a.clone(), DELTA_T);
-                }
+        for (key, celestial) in &mut self.celestials {
+            if let Some(a) = accelerations.get(key) {
+                celestial.apply_gravity(a.clone(), DELTA_T);
             }
         }
     }
