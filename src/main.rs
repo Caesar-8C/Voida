af891703c3ebc24b050a7ac0c6373f29f1870a70
Oleg Kosenko <oleg.kosenko@brainlab.com>
2023-03-04 13:06:11 +0100

    move window configs to window file

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use std::time::Duration;
 use utils::Vec3;
 use world::celestials::Celestial;
 use crate::tui::Tui;
-use crate::tui::window::Window;
+use crate::tui::window;
 use crate::world::config::Config;
 use crate::world::World;
 
@@ -16,33 +16,9 @@ async fn main() {
     let (mut world, world_watch) = World::from_config(config);
 
     let mut tui = Tui::init(world_watch, 20).await;
-    let sun_view = Window {
-        width: 80,
-        height: 53,
-        x: 0,
-        y: 0,
-        scale: 10_f64 / 10_f64.powi(11),
-        focus: "Sun".to_string(),
-    };
-    let earth_view = Window {
-        width: 80,
-        height: 40,
-        x: 81,
-        y: 0,
-        scale: 10_f64 / 3_f64 / 10_f64.powi(8),
-        focus: "Earth".to_string(),
-    };
-    let moon_view = Window{
-        width: 80,
-        height: 12,
-        x: 81,
-        y: 41,
-        scale: 10_f64 / 3_f64 / 10_f64.powi(8),
-        focus: "Moon".to_string(),
-    };
-    tui.add_window(sun_view);
-    tui.add_window(earth_view);
-    tui.add_window(moon_view);
+    tui.add_window(window::sun_standard());
+    tui.add_window(window::earth_standard());
+    tui.add_window(window::moon_from_side());
     tokio::spawn(tui.run());
 
     let simulation_period = Duration::from_millis(10);
--- src/tui/window.rs
+++ src/tui/window.rs
@@ -7,3 +7,36 @@ pub struct Window {
     pub scale: f64,
     pub focus: String,
 }
+
+pub fn sun_standard() -> Window {
+    Window {
+        width: 80,
+        height: 53,
+        x: 0,
+        y: 0,
+        scale: 10_f64 / 10_f64.powi(11),
+        focus: "Sun".to_string(),
+    }
+}
+
+pub fn earth_standard() -> Window {
+    Window {
+        width: 80,
+        height: 40,
+        x: 81,
+        y: 0,
+        scale: 10_f64 / 3_f64 / 10_f64.powi(8),
+        focus: "Earth".to_string(),
+    }
+}
+
+pub fn moon_from_side() -> Window {
+    Window {
+        width: 80,
+        height: 12,
+        x: 81,
+        y: 41,
+        scale: 10_f64 / 3_f64 / 10_f64.powi(8),
+        focus: "Moon".to_string(),
+    }
+}
