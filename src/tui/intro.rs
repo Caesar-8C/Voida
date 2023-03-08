67fbdf71d33cc3f957f202dd3db79a1ebfcfa81f
Oleg Kosenko <oleg.kosenko@brainlab.com>
2023-03-08 14:58:14 +0100

    tui/intro: optimized, moved logo and particle into submodules

--- src/tui/intro.rs
+++ src/tui/intro.rs
@@ -1,103 +1,37 @@
+mod particle;
+mod logo;
+
 use crate::tui::frame::Frame;
-use rand::Rng;
 use rodio::{Decoder, OutputStream, Sink};
 use std::fs::File;
 use std::time::Duration;
 use termion::terminal_size;
 use tokio::time::{interval, Instant};
-
-const SYMBOLS: [&str; 6] = ["#", "o", ".", ",", "*", "$"];
-const NAME: &str =
-"                                                              dddddddd                  |
-VVVVVVVV           VVVVVVVV                 iiii              d::::::d                  |
-V::::::V           V::::::V                i::::i             d::::::d                  |
-V::::::V           V::::::V                 iiii              d::::::d                  |
-V::::::V           V::::::V                                   d:::::d                   |
- V:::::V           V:::::V  ooooooooooo   iiiiiii     ddddddddd:::::d   aaaaaaaaaaaaa   |
-  V:::::V         V:::::V oo:::::::::::oo i:::::i   dd::::::::::::::d   a::::::::::::a  |
-   V:::::V       V:::::V o:::::::::::::::o i::::i  d::::::::::::::::d   aaaaaaaaa:::::a |
-    V:::::V     V:::::V  o:::::ooooo:::::o i::::i d:::::::ddddd:::::d            a::::a |
-     V:::::V   V:::::V   o::::o     o::::o i::::i d::::::d    d:::::d     aaaaaaa:::::a |
-      V:::::V V:::::V    o::::o     o::::o i::::i d:::::d     d:::::d   aa::::::::::::a |
-       V:::::V:::::V     o::::o     o::::o i::::i d:::::d     d:::::d  a::::aaaa::::::a |
-        V:::::::::V      o::::o     o::::o i::::i d:::::d     d:::::d a::::a    a:::::a |
-         V:::::::V       o:::::ooooo:::::o i::::i d::::::ddddd::::::d a::::a    a:::::a |
-          V:::::V        o:::::::::::::::o i::::i  d::::::::::::::::d a:::::aaaa::::::a |
-           V:::V          oo:::::::::::oo i::::::i  d:::::::::ddd::::d a::::::::::aa:::a|
-            VVV             ooooooooooo   iiiiiiii   ddddddddd   ddddd  aaaaaaaaaa  aaaa|";
-const NAME_X: usize = 88;
-const NAME_Y: usize = 17;
-
-struct Particle {
-    pub x: f64,
-    pub y: f64,
-    pub angle: f64,
-    pub speed: f64,
-    pub symbol: String,
-}
-
-impl Particle {
-    pub fn spawn_into(
-        container: &mut Vec<Particle>,
-        amount: usize,
-        x: usize,
-        y: usize,
-        start: Instant,
-    ) {
-        for _ in 0..amount {
-            container.push(Particle::spawn(x, y, start));
-        }
-    }
-
-    pub fn spawn(x: usize, y: usize, start: Instant) -> Self {
-        let mut rng = rand::thread_rng();
-        let mut angle: f64 = rng.gen();
-        angle *= std::f64::consts::TAU;
-
-        let random: f64 = rng.gen();
-        let index = (random * SYMBOLS.len() as f64) as usize;
-
-        let speed = if start.elapsed() > Duration::from_millis(500) {
-            let mut speed: f64 = rng.gen();
-            speed = speed * 2. + 1.;
-            speed
-        } else {
-            2.
-        };
-
-        Self {
-            x: x as f64 / 2.,
-            y: y as f64 / 2.,
-            angle,
-            speed,
-            symbol: SYMBOLS[index].to_string(),
-        }
-    }
-
-    pub fn fly(&mut self) {
-        self.x += self.speed * self.angle.cos() * 2.;
-        self.y += self.speed * self.angle.sin() / 2.;
-    }
-}
+use crate::tui::intro::logo::Logo;
+use crate::tui::intro::particle::Particle;
 
 pub struct Intro {
     frame: Frame,
     fps: u32,
     duration: Duration,
     particles: Vec<Particle>,
+    logo: Logo,
 }
 
 impl Intro {
     pub fn new(duration: Duration, fps: u32) -> Result<Self, String> {
-        let (x, y) = terminal_size().map_err(|e| format!("{}", e))?;
-        let frame = Frame::new(x as usize, y as usize - 1);
+        let (x, y) = Self::get_terminal_size()?;
+        let frame = Frame::new(x, y);
         let particles = Vec::with_capacity(1000);
+        let mut logo = Logo::voida();
+        logo.frame_center(x, y);
 
         Ok(Self {
             frame,
             fps,
             duration,
             particles,
+            logo,
         })
     }
 
@@ -114,15 +48,15 @@ impl Intro {
             start,
         );
 
+        let (x, y) = Self::get_terminal_size()?;
+        self.draw_full_frame(x, y);
+
         let mut interval = interval(Duration::from_millis(
             (1. / self.fps as f64 * 1000.) as u64,
         ));
 
         loop {
             interval.tick().await;
-            let (x, y) = terminal_size().map_err(|e| format!("{}", e))?;
-            self.frame.resize(x as usize, y as usize - 1);
-            self.frame.fill(" ".to_string());
 
             Particle::spawn_into(
                 &mut self.particles,
@@ -132,8 +66,12 @@ impl Intro {
                 start,
             );
 
-            self.draw_particles();
-            self.draw_logo();
+            let (x, y) = Self::get_terminal_size()?;
+            if self.terminal_size_changed(x, y) {
+                self.draw_full_frame(x, y);
+            } else {
+                self.redraw_frame();
+            }
 
             self.frame.flush();
 
@@ -142,6 +80,25 @@ impl Intro {
             }
         }
         Ok(())
+    }
+
+    fn terminal_size_changed(&mut self, x: usize, y: usize) -> bool {
+        x != self.frame.width || y != self.frame.height
+    }
+
+    fn draw_full_frame(&mut self, x: usize, y: usize) {
+        self.particles.clear();
+        Particle::spawn_into(
+            &mut self.particles,
+            30,
+            self.frame.width,
+            self.frame.height,
+            Instant::now(),
+        );
+
+        self.frame = Frame::new(x, y);
+        self.draw_particles();
+        self.draw_logo();
     }
 
     fn draw_particles(&mut self) {
@@ -160,21 +117,61 @@ impl Intro {
     }
 
     fn draw_logo(&mut self) {
-        let name: Vec<char> = NAME.to_string().chars().collect();
-        let start_x =
-            (self.frame.width as f64 / 2. - NAME_X as f64 / 2.) as usize;
-        let start_y =
-            (self.frame.height as f64 / 2. - NAME_Y as f64 / 2.) as usize;
+        let name: Vec<char> = self.logo.name().chars().collect();
+        self.logo.frame_center(self.frame.width, self.frame.height);
+        let (start_x, start_y, width, height) = self.logo.get_params();
 
-        for i in 0..NAME_Y {
-            for j in 0..NAME_X {
-                let index = i * (NAME_X + 2) + j;
+        for i in 0..height {
+            for j in 0..width {
+                let index = i * (width + 2) + j;
                 if self.frame.inside(j + start_x, i + start_y) {
                     self.frame.vec[i + start_y][j + start_x] =
                         name[index].to_string();
                 }
             }
         }
+    }
+
+    fn redraw_frame(&mut self) {
+        self.redraw_particles();
+    }
+
+    fn redraw_particles(&mut self) {
+        self.particles.retain_mut(|p|
+            if p.x < 0.
+                || p.y < 0.
+                || !self.frame.inside(p.x as usize, p.y as usize)
+            {
+                false
+            } else {
+                let x = p.x as usize;
+                let y = p.y as usize;
+                if !self.logo.inside(x, y) {
+                    self.frame.vec[y][x] = " ".to_string();
+                }
+
+                p.fly();
+
+                if p.x < 0.
+                    || p.y < 0.
+                    || !self.frame.inside(p.x as usize, p.y as usize)
+                {
+                    false
+                } else {
+                    let x = p.x as usize;
+                    let y = p.y as usize;
+                    if !self.logo.inside(x, y) {
+                        self.frame.vec[y][x] = p.symbol.clone();
+                    }
+                    true
+                }
+            }
+        );
+    }
+
+    fn get_terminal_size() -> Result<(usize, usize), String> {
+        let (x, y) = terminal_size().map_err(|e| format!("{}", e))?;
+        Ok((x as usize, y as usize - 1))
     }
 
     async fn play_sound(
---
+++ src/tui/intro/logo.rs
@@ -0,0 +1,53 @@
+pub struct Logo {
+    name: String,
+    width: usize,
+    height: usize,
+    x: usize,
+    y: usize,
+}
+
+impl Logo {
+    pub fn voida() -> Self {
+        Self {
+            name:
+"                                                              dddddddd                  |
+VVVVVVVV           VVVVVVVV                 iiii              d::::::d                  |
+V::::::V           V::::::V                i::::i             d::::::d                  |
+V::::::V           V::::::V                 iiii              d::::::d                  |
+V::::::V           V::::::V                                   d:::::d                   |
+ V:::::V           V:::::V  ooooooooooo   iiiiiii     ddddddddd:::::d   aaaaaaaaaaaaa   |
+  V:::::V         V:::::V oo:::::::::::oo i:::::i   dd::::::::::::::d   a::::::::::::a  |
+   V:::::V       V:::::V o:::::::::::::::o i::::i  d::::::::::::::::d   aaaaaaaaa:::::a |
+    V:::::V     V:::::V  o:::::ooooo:::::o i::::i d:::::::ddddd:::::d            a::::a |
+     V:::::V   V:::::V   o::::o     o::::o i::::i d::::::d    d:::::d     aaaaaaa:::::a |
+      V:::::V V:::::V    o::::o     o::::o i::::i d:::::d     d:::::d   aa::::::::::::a |
+       V:::::V:::::V     o::::o     o::::o i::::i d:::::d     d:::::d  a::::aaaa::::::a |
+        V:::::::::V      o::::o     o::::o i::::i d:::::d     d:::::d a::::a    a:::::a |
+         V:::::::V       o:::::ooooo:::::o i::::i d::::::ddddd::::::d a::::a    a:::::a |
+          V:::::V        o:::::::::::::::o i::::i  d::::::::::::::::d a:::::aaaa::::::a |
+           V:::V          oo:::::::::::oo i::::::i  d:::::::::ddd::::d a::::::::::aa:::a|
+            VVV             ooooooooooo   iiiiiiii   ddddddddd   ddddd  aaaaaaaaaa  aaaa|".to_string(),
+            width: 88,
+            height: 17,
+            x: 0,
+            y: 0,
+        }
+    }
+
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn frame_center(&mut self, frame_width: usize, frame_height: usize) {
+        self.x = (frame_width as f64 / 2. - self.width as f64 / 2.) as usize;
+        self.y = (frame_height as f64 / 2. - self.height as f64 / 2.) as usize;
+    }
+
+    pub fn get_params(&self) -> (usize, usize, usize, usize) {
+        (self.x, self.y, self.width, self.height)
+    }
+
+    pub fn inside(&self, x: usize, y: usize) -> bool {
+        x >= self.x && x < self.x + self.width && y >= self.y && y < self.y + self.height
+    }
+}
