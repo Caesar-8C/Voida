b8e9c54adb5190d3ec33b4c588d7759fa2a85c3e
Oleg Kosenko <oleg.kosenko@brainlab.com>
2024-05-13 15:09:37 +0200

    Cargo: update toolchain, add rapier dependency

--- src/world/spaceship.rs
+++ src/world/spaceship.rs
@@ -3,16 +3,16 @@ use crate::Vec3;
 #[derive(Clone, Debug)]
 pub struct Spaceship {
     name: String,
-    _mass: f64,
+    mass: f64,
     pos: Vec3,
     vel: Vec3,
 }
 
 impl Spaceship {
-    pub fn new(name: String, _mass: f64, pos: Vec3, vel: Vec3) -> Self {
+    pub fn new(name: String, mass: f64, pos: Vec3, vel: Vec3) -> Self {
         Self {
             name,
-            _mass,
+            mass,
             pos,
             vel,
         }
@@ -26,8 +26,8 @@ impl Spaceship {
         self.pos.clone()
     }
 
-    pub fn _mass(&self) -> f64 {
-        self._mass
+    pub fn mass(&self) -> f64 {
+        self.mass
     }
 
     pub fn apply_gravity(&mut self, acceleration: Vec3, delta_t: f64) {
